b4a45c668350290979e75051412cc939699cdd98
tania <taniaosano15@gmail.com>
2022-10-26 18:02:39 +0300

    Done

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,18 @@
-class Student 
+#class Student inherits from User (its parent class)
+class Student < User
 
+    #defining method knowledge
+    attr_accessor :knowledge
+
+    #initialize method with an empty @knowledge array
+    def initialize
+        @knowledge = []
+    end
+
+    #adds an argument of string to @knowledge array
+    def learn(strings)
+        @knowledge << strings
+    end
+  
 end
+
